959106f5e895fe95902ad5879590e4dc733e1950
IALY <ialyfrancisco@gmail.com>
2024-03-19 06:28:27 +0300

    Mon premier push dans cet dépôt, des vue, routes sont déjàs configuré et ajouté dans le projet. La page d'accueil et la page d'administration y sont déjà disponibles

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Définir EJS comme moteur de modèle
+app.set('view engine', 'ejs');
+
+// Définir le chemin vers le répertoire contenant les vues
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+// Route pour afficher le modèle de vue admin_space.ejs
+app.get('/admin', (req, res) => {
+  res.render('back_office');
+});
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/my_skills', (req, res) => {
+  res.render('my_skills');
+})
+
+app.get('/my_projects', (req, res) => {
+  res.render('my_projects');
+})
+
+// Définir le répertoire des fichiers statiques
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+// Démarrer le serveur
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur en écoute sur le port ${PORT}`);
+});
