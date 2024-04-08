a65f780db4916dc6b3a7b9029b22d9f2c32e59a0
IALY <ialyfrancisco@gmail.com>
2024-04-08 22:01:57 +0300

    ajout de contenus dans la section my skills du backoffice

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ app.set('view engine', 'ejs');
 // Définir le chemin vers le répertoire contenant les vues
 app.set('views', path.join(__dirname, 'src', 'views'));
 
-// Route pour afficher le modèle de vue admin_space.ejs
-app.get('/back_office', (req, res) => {
-  res.render('back_office');
-});
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -55,11 +51,70 @@ app.get('/my_contacts', (request, response) => {
 app.get('/testPage', (request, response) => {
 
   const student_list = new Array('IALY', 'Francisco', 'Raymond');
-
+  
   response.render('testPage', { student_list : student_list });
+  
+});
 
-})
+app.get('/back_office', (req, res) => {
+  res.render('back_office');
+});
 
+app.get('/BO_my_skills', (req, res) => {
+
+  let list_of_my_skills = new Array(
+    {
+      skill_image_logo_file_url : "assets/icon/icons8-javascript-color/icons8-javascript-100.png",
+      skill_name : "javascript",
+      knowledge_level : 60,
+      known_library : [
+        "jQuery"
+      ]
+    },
+    {
+      skill_image_logo_file_url : "assets/icon/icons8-angular-color/icons8-angular-100.png",
+      skill_name : "angular",
+      knowledge_level : 40,
+      known_library : [
+        "httpClient",
+        "rxjs",
+        "material",
+      ]
+    },
+    {
+      skill_image_logo_file_url : "assets/icon/icons8-vue-js-color/icons8-vue-js-100.png",
+      skill_name : "vue",
+      knowledge_level : 37,
+      known_library : [
+        "vuex",
+        "pinia",
+        "ionic",
+        "vuetify"
+      ]
+    },
+    {
+      skill_image_logo_file_url : "assets/icon/icons8-mongodb-a-cross-platform-document-oriented-database-program-color/icons8-mongodb-a-cross-platform-document-oriented-database-program-96.png",
+      skill_name : "mongodb",
+      knowledge_level : 36,
+      known_library : [
+      ]
+    },
+    {
+      skill_image_logo_file_url : "assets/icon/icons8-nodejs-color/icons8-nodejs-100.png",
+      skill_name : "node",
+      knowledge_level : 40,
+      known_library : [
+        "http",
+        "mysql",
+        "mongoose",
+        "express",
+        "ejs"
+      ]
+    },
+  )
+
+  res.render('BO_my_skills', { data : list_of_my_skills });
+});
 
 // Définir le répertoire des fichiers statiques
 app.use(express.static(path.join(__dirname, 'src/public')));
