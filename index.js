19e94aaa4322708382b6dd02f3c078dfbbcb4dd0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2024-08-01 15:02:33 +0300

    ajout d'un icon dans le sidebar du dashboard

--- index.js
+++ index.js
@@ -102,5 +102,5 @@ app.get('/BO_my_skills', (req, res) => {
 });
 
 app.listen(process.env.APP_PORT, () => {
-  console.log(`Serveur en écoute sur le port ${process.env.APP_PORT}`);
+  console.log(`http://127.0.0.1:${process.env.APP_PORT}`);
 });
