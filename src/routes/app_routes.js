5074e3a6eedb0ff3dabd5613c437e1d90c8ee0a4
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2024-08-17 09:05:38 +0300

    restructuration du projet, ajout de routes et controller du projet

--- src/routes/app_routes.js
+++ src/routes/app_routes.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app_routes = express.Router()
+const goToMySkills = require('../controllers/app_controllers');
+const goToMyContacts = require('../controllers/app_controllers');
+const goToBackoffice = require('../controllers/app_controllers');
+const goToBackofficeMySkills = require('../controllers/app_controllers');
   
-app_routes.get('/my_skills', (req, res) => {
-    res.render('my_skills');
-})
-//Mauvaise exportation c'est module export, non pas exports.modules
-// exports.modules = app_routes
+app_routes.get('/my_skills', goToMySkills)
+
+app_routes.get('/my_contacts', goToMyContacts);
+  
+app_routes.get('/backoffice', goToBackoffice);
+  
+app_routes.get('/BO_my_skills', goToBackofficeMySkills);  
+
 module.exports = app_routes
 
