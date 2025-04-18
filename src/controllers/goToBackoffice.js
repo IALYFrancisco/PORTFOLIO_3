6aff90f8d3c28bb59c4dd91b06bee06058339b15
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-18 14:12:56 +0300

    dynamisation de certaine partie du backoffice

--- src/controllers/goToBackoffice.js
+++ src/controllers/goToBackoffice.js
@@ -10,7 +10,7 @@ async function goToBackoffice(request, response) {
     }else{
         let list_of_my_projects = await getAllProjects({})
         let context = {
-            projectsList: list_of_my_projects
+            projectsList: list_of_my_projects,
         }
         response.render('backoffice', context)
     }
