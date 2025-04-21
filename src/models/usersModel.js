89b30fb0a2a0f68719a21da1979880e3906bfa92
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-21 21:46:38 +0300

    amélioration du medèle de données des utilisateurs

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const usersSchema = mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: "user" }
 })
