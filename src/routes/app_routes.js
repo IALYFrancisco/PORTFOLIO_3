759416764b201db5fae886002bc01ee8d42be20d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-21 11:08:15 +0300

    changement du design du formulaire d'ajout de projet

--- src/routes/app_routes.js
+++ src/routes/app_routes.js
@@ -4,10 +4,6 @@ const goToMySkills = require('../controllers/goToMySkillsController')
 const goToMyContacts = require('../controllers/goToMyContactsController')
 const goToHome = require('../controllers/goToHomeController')
 const goToMyProjects = require('../controllers/goToMyProjectsController')
-const goToBackoffice = require('../controllers/goToBackoffice')
-const goToBackofficeMySkills = require('../controllers/goToBackofficeMySkills')
-const goToAddProject = require('../controllers/ProjectController')
-const addProject = require('../services/addProjectService')
 
 app_routes.get('/', goToHome)
 
@@ -16,7 +12,5 @@ app_routes.get('/my-skills', goToMySkills)
 app_routes.get('/my-contacts', goToMyContacts);
 
 app_routes.get('/my-projects', goToMyProjects)
-
-app_routes.post('/add-project', addProject)
 
 module.exports = app_routes
--- src/routes/backoffice_routes.js
+++ src/routes/backoffice_routes.js
@@ -2,11 +2,14 @@ const express = require('express')
 const goToBackoffice = require('../controllers/goToBackoffice')
 const goToAddProject = require('../controllers/ProjectController')
 const goToBackofficeMySkills = require('../controllers/goToBackofficeMySkills')
+const addProjectService = require('../services/addProjectService')
 const _backoffice_routes = express.Router()
 
 _backoffice_routes.get('/', goToBackoffice)
 
 _backoffice_routes.get('/add-project', goToAddProject)
+
+_backoffice_routes.post('/add-project', addProjectService)
 
 _backoffice_routes.get('/my-skills', goToBackofficeMySkills)
 
