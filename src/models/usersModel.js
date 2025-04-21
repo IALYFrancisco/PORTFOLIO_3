8dbcf252be4a4ec27765ba7ae9bfb5a365ebb1d2
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-21 12:22:33 +0300

    définition du modèle de données des utilisateurs

---
+++ src/models/usersModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const usersSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "user" }
+})
+
+const _Users = new mongoose.mode('Users', usersSchema)
+
+module.exports = { Users : _Users }
