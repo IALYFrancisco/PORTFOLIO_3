3ef357e09bb7145c93702cd8cff4fe5b17dd068d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-21 12:40:34 +0300

    configuration des sessions et des messages flash de l'application

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv')
 const app_routes = require('./src/routes/app_routes')
 const auth_routes = require('./src/routes/auth_routes')
 const backoffice_routes = require('./src/routes/backoffice_routes')
+const session = require('express-session')
+const flash = require('connect-flash')
 const app = express();
 
 dotenv.config();
@@ -16,6 +18,21 @@ app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.use(body_parser.urlencoded({extended:true}))
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(flash())
+
+app.use((request, response, next) => {
+  response.locals.error = request.flash('error')
+  response.locals.success = request.flash('success')
+  response.locals.user = request.session.user
+  next()
+})
 
 app.use('/', app_routes)
 
