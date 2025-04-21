570d419fb738230972482d1baded989c250d6c53
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-21 21:44:40 +0300

    ajout de méthode de connexion et de déconnexion à la base de données

--- src/services/addProjectService.js
+++ src/services/addProjectService.js
@@ -1,9 +1,12 @@
 const projet = require('../models/projectsModel')
+const { connection, disconnection } = require('../services/db')
 
 async function addProjectService(request, response) {
     try{
+        await connection()
         let newProject = projet(request.body)
         await newProject.save()
+        await disconnection()
         response.redirect('/backoffice')
     }catch(error){
         console.log('There are an errors: ' + error)
