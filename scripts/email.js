a605b7e3998b6c81955a3202785fe30bbcdb9b45
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-29 15:19:15 +0300

    changement du template d'email

--- scripts/email.js
+++ scripts/email.js
@@ -8,17 +8,41 @@ async function _send_email(password){
     try{
 
         let emaiTemplateHTML = `
-            <section>
-                <h1>Superuser PORTFOLIO_3 login:</h1>
-                <ul>
-                    <li class="superuser_email">
-                        ${process.env.SUPERUSER_EMAIL}
-                    </li>
-                    <li class="superuser_password">
-                        ${password}
-                    </li>
-                </ul>
-            </section>
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <main style="width: 100%; height: max-content;">
+                    <section style="width: 100%; max-width: 500px; margin: 100px auto;">
+                        <header style="height: 50px; width: 100%; background-color: #581845; border-top-right-radius: 10px; border-top-left-radius: 10px;"></header>
+                        <section style="padding: 50px 25px 25px 25px">
+                            <h2 style="font-family: 'Trebuchet MS', Arial, sans-serif;">Superuser informations:</h2>
+                        </section>
+                        <section style="padding: 0 25px; margin-top: 15px; margin-bottom: 50px;">
+                            <div style="display: flex; align-items: center;">
+                                <h4 style="font-family: 'Trebuchet MS', Arial, sans-serif; margin: 0; padding: 0;">Name: </h4><p style="font-family: 'Trebuchet MS', Arial, sans-serif; margin-left: 10px;">${process.env.SUPERUSER_NAME}</p>
+                            </div>
+                            <div style="display: flex; align-items: center; margin-top: 15px; padding: 0;">
+                                <h4 style="font-family: 'Trebuchet MS', Arial, sans-serif; margin: 0; padding: 0;">Email: </h4><p style="font-family: 'Trebuchet MS', Arial, sans-serif; margin-left: 10px;">${process.env.SUPERUSER_EMAIL}</p>
+                            </div>
+                            <div style="display: flex; align-items: center; margin-top: 15px; padding: 0;">
+                                <h4 style="font-family: 'Trebuchet MS', Arial, sans-serif; margin: 0; padding: 0;">Initial password: </h4><p style="font-family: 'Trebuchet MS', Arial, sans-serif; margin-left: 10px;">${password}</p>
+                            </div>
+                            <div style="margin-top: 25px;">
+                                <a href="https://portfolio-4n0j.onrender.com/authentication/login" target="_blank">
+                                    <button style="padding: 10px 13px;background-color: yellow;border: none;border-radius: 5px;box-shadow:1px 1px 10px #9797972d, -1px -1px 10px #90909035;cursor: pointer;color: #000;font-weight: bold;transition: 0.1s;outline: none;">Login</button>
+                                </a>
+                            </div>
+                        </section>
+                        <footer  style="height: 50px; width: 100%; background-color: #581845; border-bottom-right-radius: 10px; border-bottom-left-radius: 10px;"></footer>
+                    </section>
+                </main>
+            </body>
+            </html>
         `
 
         let EMAIL = {
