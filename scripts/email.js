d1c188c48fecde7232ee376103dc6da8ae837f2b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-29 08:15:03 +0300

    mis à jour du service d'envoie d'email

--- scripts/email.js
+++ scripts/email.js
@@ -4,12 +4,41 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-async function _send_email(email){
+async function _send_email(password){
     try{
+
+        let emaiTemplateHTML = `
+            <section>
+                <h1>Superuser PORTFOLIO_3 login:</h1>
+                <ul>
+                    <li class="superuser_email">
+                        ${process.env.SUPERUSER_EMAIL}
+                    </li>
+                    <li class="superuser_password">
+                        ${password}
+                    </li>
+                </ul>
+            </section>
+        `
+
+        let EMAIL = {
+            name: "Email from PORTFOLIO_3 platform.",
+            subject: "Superuser creation.",
+            sender : {
+                name: "PORTFOLIO_3",
+                email: "franciscoialy43@gmail.com"
+            },
+            to: [{
+                name: `${process.env.SUPERUSER_NAME}`,
+                email: `${process.env.SUPERUSER_EMAIL}`
+            }],
+            htmlContent: emaiTemplateHTML
+        }
+
         await axios({
             method: 'POST',
             url: process.env.EMAIL_SERVER_URL,
-            data: email,
+            data: EMAIL,
             headers: {
                 "Content-Type" : "application/json",
                 "api-key" : process.env.EMAIL_API_KEY
