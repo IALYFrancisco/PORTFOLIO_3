ad5aa8412405606c23a73990a05b788bcabe52b2
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-18 13:45:49 +0300

    mis à jour du script de création de root

--- scripts/services/services.js
+++ scripts/services/services.js
@@ -1,3 +1,4 @@
+const { connexion, disconnexion } = require("../../src/Services/DbServices")
 const { Users } = require('../../src/Models/UsersModel')
 const fs = require('fs')
 const os = require('os')
@@ -5,7 +6,9 @@ const bcrypt = require('bcrypt')
 const path = require('path')
 const { default: axios } = require("axios");
 const chalk = require('chalk')
-const { disconnection, connection } = require('../../src/services/db')
+const crypto = require('crypto')
+
+var root_password = crypto.randomBytes(32).toString('hex')
 
 var userToCreate = {
     name: null,
@@ -15,14 +18,14 @@ var userToCreate = {
 }
 
 async function checkSuperuser(){
-    await connection()
+    await connexion()
     let user = await Users.findOne({email : process.env.SUPERUSER_EMAIL})
     if(user){
-        await disconnection()
+        await disconnexion()
         return true
     }
     else{
-        await disconnection()
+        await disconnexion()
         return false
     }
 }
@@ -30,11 +33,11 @@ async function checkSuperuser(){
 async function createSuperuser(){
     try{
         console.log(chalk.yellow("Creating superuser ..."))
-        userToCreate.password = await hashpassword(`${Math.PI}`)
-        await connection()
+        userToCreate.password = await hashpassword(`${root_password}`)
+        await connexion()
         let user = Users(userToCreate)
         await user.save()
-        await disconnection()
+        await disconnexion()
         return true
     }catch(err){
         console.log({
@@ -48,13 +51,13 @@ async function createSuperuser(){
 async function save_local(u,p){
     try {
         let homeDir = os.homedir()
-        let dossierSuperuser = path.join(homeDir, '.ialy', 'superuser')
+        let dossierSuperuser = path.join(homeDir, '.me', 'superuser')
         fs.mkdirSync(dossierSuperuser, { recursive: true })
         let fileContents =
         `{"name":"${u.name}","email":"${u.email}","password":"${p}"}`
         let filePath = path.join(dossierSuperuser, 'informations.json')
         fs.writeFileSync(filePath, fileContents, 'utf-8')
-        console.log(chalk.yellow(`Superuser informations is saved at ${filePath}`))
+        console.log(chalk.yellow(`Superuser informations are saved at ${filePath}`))
         console.log(chalk.bgGreenBright.black('Done!'))
     }catch(err) {
         console.log({
@@ -168,7 +171,7 @@ async function _LDOTASK(){
         let results = await createSuperuser()
         if(results){
             console.log(chalk.yellow("Local informations saving ..."))
-            await save_local(userToCreate, Math.PI)
+            await save_local(userToCreate, root_password)
         }
     }
 
@@ -199,7 +202,7 @@ async function _EDOTASK(){
         let results = await createSuperuser()
         if(results){
             console.log("Sending by email ...")
-            await send_email(Math.PI)
+            await send_email(root_password)
         }
     }
 
