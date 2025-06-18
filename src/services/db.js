061891ba5e98c0b420a7899e64a705908455b56c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-18 13:47:00 +0300

    amélioration du service de connexion à la base de données

--- src/services/db.js
+++ src/services/db.js
@@ -3,10 +3,17 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
+let isConnected = false
+
 async function _Connection() {
-    await mongoose.connect(process.env.DB_URI)
-        .then(()=>{console.log("Connection database OK")})
-        .catch((error)=>{console.log(error)})
+    if(!isConnected){
+        await mongoose.connect(process.env.DB_URI)
+            .then(()=>{
+                isConnected = true
+                console.log("Connection database OK")
+            })
+            .catch((error)=>{console.log(error)})
+    }
 }
 
 async function _Disconnection() {
