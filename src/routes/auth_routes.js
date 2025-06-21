d04c121973072d38d4c85585ce97019be1e3a31a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-21 20:14:21 +0300

    route register pour la méthode POST définie

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { goToLogin, checkLogin, logout, zappLogin, _Register } = require('../services/auth_services')
+const { goToLogin, checkLogin, logout, zappLogin, _Register, Register } = require('../services/auth_services')
 const _auth_routes = express.Router()
 
 _auth_routes.get('/login', zappLogin, goToLogin)
@@ -9,6 +9,8 @@ _auth_routes.post('/login', checkLogin)
 _auth_routes.get('/logout', logout)
 
 _auth_routes.get('/register', _Register)
+
+_auth_routes.post('/register', Register)
 
 module.exports = {
     auth_routes : _auth_routes
