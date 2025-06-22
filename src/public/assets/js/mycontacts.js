ef611068e67f4eb80d8ec81b33aca96b516711cf
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-22 19:47:49 +0300

    copie colle de style et script du dropdown dans la page my-contacts

---
+++ src/public/assets/js/mycontacts.js
@@ -0,0 +1,7 @@
+var profile = document.querySelector('.border')
+var dropdown = document.querySelector('.dropdown-container')
+
+profile.addEventListener('click', () => {
+   console.log(dropdown.classList)
+   dropdown.classList.toggle('active')
+})
