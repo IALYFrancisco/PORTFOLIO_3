9a3e24c39ba2b86f7a0b4d6f8ff686447ca2d0f0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-22 19:20:51 +0300

    suppression du service getAllProjectsService.js, il est inutile

--- src/routes/backoffice_routes.js
+++ src/routes/backoffice_routes.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const goToBackoffice = require('../controllers/goToBackoffice')
 const goToAddProject = require('../controllers/ProjectController')
 const goToBackofficeMySkills = require('../controllers/goToBackofficeMySkills')
 const addProjectService = require('../services/addProjectService')
 const { AuthenticatedAndAdmin } = require('../services/auth_services')
+const { goToBackoffice } = require('../services/backoffice')
 const _backoffice_routes = express.Router()
 
 _backoffice_routes.use(AuthenticatedAndAdmin)
--- src/services/getAllProjectsService.js
+++
@@ -1,8 +0,0 @@
-const projectCollection = require('../models/projectsModel')
-
-async function getAllProjects(filters) {
-  const all_projects = await projectCollection.find(filters)
-  return all_projects
-}
-
-module.exports = getAllProjects
