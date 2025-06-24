0ec13f7abaf584f5ba6c35a798ecebe2805f6bf5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-24 16:11:28 +0300

    ajout du middleware Authenticated dans les routes préfixées par /user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,11 @@
+const { Authenticated } = require('../services/auth_services')
 const { UploadProfile, Upload } = require('../services/user')
 
 const express = require('express')
 
 const _user_router = express.Router()
 
-_user_router.post('/profile', , Upload.single('profile'), UploadProfile)
+_user_router.post('/profile', Authenticated, Upload.single('profile'), UploadProfile)
 
 module.exports = {
     user_router: _user_router
