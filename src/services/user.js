c4e74c9ffc6d364852e73a0d1a027c0ec53bc244
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-24 14:02:13 +0300

    configuration du mode de stockage des images de profiles

--- src/services/user.js
+++ src/services/user.js
@@ -1,3 +1,19 @@
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+const router = express.Router()
+
+const storage = multer.diskStorage({
+    destination: (request, file, cb) => {
+        cb(null, '../public/uploads/pofiles')
+    },
+    filename: (request, file, cb) => {
+        const ext = path.extname(file.originalname)
+        const uniqueName = Date.now() + '-' + Math.round(Math.random * 1E9) + ext
+        cb(null, uniqueName)
+    }
+})
+
 function UploadProfile(request, response){
-    
+
 }
