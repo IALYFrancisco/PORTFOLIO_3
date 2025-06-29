552e5ec80f8141a9c863bd3d0b5e7b290e899e83
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-29 14:13:05 +0300

    ajout d'attribut logo au modèle de donnée Skills

--- src/models/Skills.js
+++ src/models/Skills.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const skillsSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    logo: { type: String, required: true },
     category: { type: String, required: true },
     knowledge_percentage: { type: Number, default: 0 },
     add_date: { type: Date, default: Date.now }
