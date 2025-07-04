37c5287f1b182292543d7e73368aa5b482e25d1c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-04 13:48:39 +0300

    copie colle du style dans la page my skilles

--- src/public/assets/js/myskills.js
+++ src/public/assets/js/myskills.js
@@ -1,9 +1,15 @@
 var profile = document.querySelector('.border')
 var dropdown = document.querySelector('.dropdown-container')
 
-if(profile){
-   profile.addEventListener('click', () => {
-      console.log(dropdown.classList)
-      dropdown.classList.toggle('active')
+var overlay = document.querySelector('.overlay')
+
+if(overlay){
+   document.getElementById('toggleBtn').addEventListener('click', () => {
+      overlay.classList.add('active')
+      document.querySelector(".overlay ul").classList.add('active')
+   })
+   document.querySelector('.close').addEventListener('click', () => {
+      overlay.classList.remove('active')
+      document.querySelector(".overlay ul").classList.remove('active')
    })
 }
