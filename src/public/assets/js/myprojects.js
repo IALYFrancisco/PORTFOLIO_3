e937745ef4193e2849f78b00b7cf68344ce01f8a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-04 13:52:22 +0300

    copie colle du style dans la page my projects

--- src/public/assets/js/myprojects.js
+++ src/public/assets/js/myprojects.js
@@ -1,6 +1,7 @@
 var seeLoreProjectBtn = document.getElementById('seeMoreProjectBtn');
 var profile = document.querySelector('.border')
 var dropdown = document.querySelector('.dropdown-container')
+var overlay = document.querySelector('.overlay')
 
 seeLoreProjectBtn.addEventListener("click", (event) => {
        
@@ -13,6 +14,17 @@ seeLoreProjectBtn.addEventListener("click", (event) => {
     textIncitation.classList.toggle("toggled");
 
 })
+
+if(overlay){
+   document.getElementById('toggleBtn').addEventListener('click', () => {
+      overlay.classList.add('active')
+      document.querySelector(".overlay ul").classList.add('active')
+   })
+   document.querySelector('.close').addEventListener('click', () => {
+      overlay.classList.remove('active')
+      document.querySelector(".overlay ul").classList.remove('active')
+   })
+}
 
 if(profile){
    profile.addEventListener('click', () => {
