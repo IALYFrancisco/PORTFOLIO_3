2ae7472755cf50289be428b73595de8664d5388e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-04 14:18:26 +0300

    copie colle du script javascript

--- src/public/assets/js/mycontacts.js
+++ src/public/assets/js/mycontacts.js
@@ -1,5 +1,17 @@
 var profile = document.querySelector('.border')
 var dropdown = document.querySelector('.dropdown-container')
+var overlay = document.querySelector('.overlay')
+
+if(overlay){
+   document.getElementById('toggleBtn').addEventListener('click', () => {
+      overlay.classList.add('active')
+      document.querySelector(".overlay ul").classList.add('active')
+   })
+   document.querySelector('.close').addEventListener('click', () => {
+      overlay.classList.remove('active')
+      document.querySelector(".overlay ul").classList.remove('active')
+   })
+}
 
 if(profile){
    profile.addEventListener('click', () => {
