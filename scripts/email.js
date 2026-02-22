ceee3d961e1d980551bbcc8cbb4e727ca708579c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-22 17:16:50 +0300

    corréction du script à part

--- scripts/email.js
+++ scripts/email.js
@@ -1,15 +1,18 @@
 const dotenv = require('dotenv')
 const { EDOTASK } = require('./services/services')
 const { DbConnection } = require('../src/services/db')
+const { disconnect } = require('mongoose')
 
 dotenv.config()
-DbConnection()
 
 try {
+    DbConnection()
     EDOTASK()
 }catch(_error){
     console.log({
         message : "Failed to create superuser.",
         error : _error
     })
+}finally{
+    await disconnect()
 }
--- scripts/local.js
+++ scripts/local.js
@@ -1,15 +1,18 @@
 const dotenv = require('dotenv')
 const { LDOTASK } = require('./services/services')
 const { DbConnection } = require('../src/services/db')
+const { disconnect } = require('mongoose')
 
 dotenv.config()
-DbConnection()
 
 try {
+    DbConnection()
     LDOTASK()
 }catch(_error){
     console.log({
         message : "Failed to create superuser.",
         error : _error
     })
+}finally{
+    await disconnect()
 }
